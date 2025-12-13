89bf4c4d63e8d123041af36ac10ea6ecfd821972
Semyon Shtelman <shtelmaan@gmail.com>
2025-12-13 13:16:10 +0600

    The str, add method has been redefined for the Product class

--- src/product.py
+++ src/product.py
@@ -10,6 +10,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return (self.__price * self.quantity) + (other.__price * other.quantity)
+
     @classmethod
     def new_product(cls, new_product):
         return cls(**new_product)
