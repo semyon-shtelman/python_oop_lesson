866bf6a97629656d14f8b13a6be39e17a8af9931
Semyon Shtelman <shtelmaan@gmail.com>
2025-12-23 03:41:18 +0600

    add fixture for smartphone and lawngrass classes

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ from src.category import Category
 from src.product import Product
 import pytest
 
+from src.smartphone import Smartphone
+from src.lawngrass import LawnGrass
+
 
 @pytest.fixture
 def new_product():
@@ -52,4 +55,65 @@ def category_2(product_4):
         "Телевизоры",
         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
         [product_4],
+    )
+
+
+@pytest.fixture
+def smartphone1():
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+        95.5,
+        "S23 Ultra",
+        256,
+        "Серый",
+    )
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone(
+        "Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"
+    )
+
+
+@pytest.fixture
+def smartphone3():
+    return Smartphone(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14,
+        90.3,
+        "Note 11",
+        1024,
+        "Синий",
+    )
+
+
+@pytest.fixture
+def grass1():
+    return LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый",
+    )
+
+
+@pytest.fixture
+def grass2():
+    return LawnGrass(
+        "Газонная трава 2",
+        "Выносливая трава",
+        450.0,
+        15,
+        "США",
+        "5 дней",
+        "Темно-зеленый",
     )
